45c580cdc652a802aa8ae5a44352e623c00ed42c
bogdanaks <bogdanaks@bk.ru>
2020-08-05 23:34:42 +0300

    Base style component

--- client/src/components/ChatMessages/ChatMessages.jsx
+++ client/src/components/ChatMessages/ChatMessages.jsx
@@ -4,7 +4,100 @@ import './styles.scss'
 const ChatMessages = () => {
     return (
         <div className="wrapperChatMessages">
-            <h1>Chat messages</h1>
+            <div className="container-fluid messagesBlock">
+                <ul>
+                    <li>
+                        <div className="row messagesBlock__message">
+                            <div className="row">
+                                <div className="col-8"><div className="col-12 messagesBlock__message__user">Bogdan Aksenov</div></div>
+                                <div className="col-4"><div className="messageTime">10:20 pm</div></div>
+                            </div>
+                            <div className="col-12 messagesBlock__message__text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto reprehenderit non ut, animi ipsam at?</div>
+                        </div>
+                    </li>
+                    <li>
+                        <div className="row messagesBlock__message messageSelf">
+                            <div className="row">
+                                <div className="col-8"><div className="col-12 messagesBlock__message__user">Bogdan Aksenov</div></div>
+                                <div className="col-4"><div className="messageTime">10:20 pm</div></div>
+                            </div>
+                            <div className="col-12 messagesBlock__message__text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto reprehenderit non ut, animi ipsam at?</div>
+                        </div>
+                    </li>
+                    <li>
+                        <div className="row messagesBlock__message">
+                            <div className="row">
+                                <div className="col-8"><div className="col-12 messagesBlock__message__user">Bogdan Aksenov</div></div>
+                                <div className="col-4"><div className="messageTime">10:20 pm</div></div>
+                            </div>
+                            <div className="col-12 messagesBlock__message__text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto reprehenderit non ut, animi ipsam at?</div>
+                        </div>
+                    </li>
+                    <li>
+                        <div className="row messagesBlock__message">
+                            <div className="row">
+                                <div className="col-8"><div className="col-12 messagesBlock__message__user">Bogdan Aksenov</div></div>
+                                <div className="col-4"><div className="messageTime">10:20 pm</div></div>
+                            </div>
+                            <div className="col-12 messagesBlock__message__text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto reprehenderit non ut, animi ipsam at?</div>
+                        </div>
+                    </li>
+                    <li>
+                        <div className="row messagesBlock__message">
+                            <div className="row">
+                                <div className="col-8"><div className="col-12 messagesBlock__message__user">Bogdan Aksenov</div></div>
+                                <div className="col-4"><div className="messageTime">10:20 pm</div></div>
+                            </div>
+                            <div className="col-12 messagesBlock__message__text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto reprehenderit non ut, animi ipsam at?</div>
+                        </div>
+                    </li>
+                    <li>
+                        <div className="row messagesBlock__message">
+                            <div className="row">
+                                <div className="col-8"><div className="col-12 messagesBlock__message__user">Bogdan Aksenov</div></div>
+                                <div className="col-4"><div className="messageTime">10:20 pm</div></div>
+                            </div>
+                            <div className="col-12 messagesBlock__message__text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto reprehenderit non ut, animi ipsam at?</div>
+                        </div>
+                    </li>
+                    <li>
+                        <div className="row messagesBlock__message">
+                            <div className="row">
+                                <div className="col-8"><div className="col-12 messagesBlock__message__user">Bogdan Aksenov</div></div>
+                                <div className="col-4"><div className="messageTime">10:20 pm</div></div>
+                            </div>
+                            <div className="col-12 messagesBlock__message__text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto reprehenderit non ut, animi ipsam at?</div>
+                        </div>
+                    </li>
+                    <li>
+                        <div className="row messagesBlock__message messageSelf">
+                            <div className="row">
+                                <div className="col-8"><div className="col-12 messagesBlock__message__user">Bogdan Aksenov</div></div>
+                                <div className="col-4"><div className="messageTime">10:20 pm</div></div>
+                            </div>
+                            <div className="col-12 messagesBlock__message__text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto reprehenderit non ut, animi ipsam at?</div>
+                        </div>
+                    </li>
+                    <li>
+                        <div className="row messagesBlock__message messageSelf">
+                            <div className="row">
+                                <div className="col-8"><div className="col-12 messagesBlock__message__user">Bogdan Aksenov</div></div>
+                                <div className="col-4"><div className="messageTime">10:20 pm</div></div>
+                            </div>
+                            <div className="col-12 messagesBlock__message__text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto reprehenderit non ut, animi ipsam at?</div>
+                        </div>
+                    </li>
+                    <li>
+                        <div className="row messagesBlock__message">
+                            <div className="row">
+                                <div className="col-8"><div className="col-12 messagesBlock__message__user">Bogdan Aksenov</div></div>
+                                <div className="col-4"><div className="messageTime">10:20 pm</div></div>
+                            </div>
+                            <div className="col-12 messagesBlock__message__text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto reprehenderit non ut, animi ipsam at?</div>
+                        </div>
+                    </li>
+                </ul>
+            </div>
         </div>
     )
 }
