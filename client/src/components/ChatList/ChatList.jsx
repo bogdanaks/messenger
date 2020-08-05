a7325865906e65bfb2956f2e537a135d817734fe
bogdanaks <bogdanaks@bk.ru>
2020-08-05 19:54:28 +0300

    Create new component - ChatList

---
+++ client/src/components/ChatList/ChatList.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './styles.scss'
+
+const ChatList = () => {
+    return (
+        <div className="wrapperChatList">
+            <div className="container-fluid">
+                <div className="row chatList__header">1</div>
+                <div className="row">2</div>
+            </div>
+        </div>
+    )
+}
+
+export default ChatList
