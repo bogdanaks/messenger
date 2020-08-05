9b7c6d98f5a52e0ad970438051f7f2cea2661792
bogdanaks <bogdanaks@bk.ru>
2020-08-05 21:20:21 +0300

    Base style component

--- client/src/components/ChatList/ChatList.jsx
+++ client/src/components/ChatList/ChatList.jsx
@@ -4,9 +4,213 @@ import './styles.scss'
 const ChatList = () => {
     return (
         <div className="wrapperChatList">
-            <div className="container-fluid">
-                <div className="row chatList__header">1</div>
-                <div className="row">2</div>
+            <div className="container-fluid p-0">
+                <div className="row chatList__header no-gutters mh-30">
+                    <h4>Chats (2)</h4>
+                    <span>+</span>
+                </div>
+                <div className="row chatList__list no-gutters mh-70">
+                    <ul className="chatList__content">
+                        <li className="chatList__list__item active">
+                            <div className="col-12 itemBlock p-0">
+                                <div className="row">
+                                    <div className="col-8">
+                                        <span className="chatId">#123123123</span>
+                                    </div>
+                                    <div className="col-4 chatTime text-right">10:20 pm</div>
+                                </div>
+                                <div className="row">
+                                    <div className="col-12 userName">Bogdan Aksenov</div>
+                                </div>
+                                <div className="row">
+                                    <div className="col-12">
+                                        <p className="userMessage">Lorem ipsum dolor sit ametw wadawd dawdawd awdawd.</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </li>
+                        <li className="chatList__list__item">
+                            <div className="col-12 itemBlock p-0">
+                                <div className="row">
+                                    <div className="col-8">
+                                        <span className="chatId">#123123123</span>
+                                    </div>
+                                    <div className="col-4 chatTime text-right">10:20 pm</div>
+                                </div>
+                                <div className="row">
+                                    <div className="col-12 userName">Bogdan Aksenov</div>
+                                </div>
+                                <div className="row">
+                                    <div className="col-12">
+                                        <p className="userMessage">Lorem ipsum dolor sit ametw wadawd dawdawd awdawd.</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </li>
+                        <li className="chatList__list__item">
+                            <div className="col-12 itemBlock p-0">
+                                <div className="row">
+                                    <div className="col-8">
+                                        <span className="chatId">#123123123</span>
+                                    </div>
+                                    <div className="col-4 chatTime text-right">10:20 pm</div>
+                                </div>
+                                <div className="row">
+                                    <div className="col-12 userName">Bogdan Aksenov</div>
+                                </div>
+                                <div className="row">
+                                    <div className="col-12">
+                                        <p className="userMessage">Lorem ipsum dolor sit ametw wadawd dawdawd awdawd.</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </li>
+                        <li className="chatList__list__item">
+                            <div className="col-12 itemBlock p-0">
+                                <div className="row">
+                                    <div className="col-8">
+                                        <span className="chatId">#123123123</span>
+                                    </div>
+                                    <div className="col-4 chatTime text-right">10:20 pm</div>
+                                </div>
+                                <div className="row">
+                                    <div className="col-12 userName">Bogdan Aksenov</div>
+                                </div>
+                                <div className="row">
+                                    <div className="col-12">
+                                        <p className="userMessage">Lorem ipsum dolor sit ametw wadawd dawdawd awdawd.</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </li>
+                        <li className="chatList__list__item">
+                            <div className="col-12 itemBlock p-0">
+                                <div className="row">
+                                    <div className="col-8">
+                                        <span className="chatId">#123123123</span>
+                                    </div>
+                                    <div className="col-4 chatTime text-right">10:20 pm</div>
+                                </div>
+                                <div className="row">
+                                    <div className="col-12 userName">Bogdan Aksenov</div>
+                                </div>
+                                <div className="row">
+                                    <div className="col-12">
+                                        <p className="userMessage">Lorem ipsum dolor sit ametw wadawd dawdawd awdawd.</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </li>
+                        <li className="chatList__list__item">
+                            <div className="col-12 itemBlock p-0">
+                                <div className="row">
+                                    <div className="col-8">
+                                        <span className="chatId">#123123123</span>
+                                    </div>
+                                    <div className="col-4 chatTime text-right">10:20 pm</div>
+                                </div>
+                                <div className="row">
+                                    <div className="col-12 userName">Bogdan Aksenov</div>
+                                </div>
+                                <div className="row">
+                                    <div className="col-12">
+                                        <p className="userMessage">Lorem ipsum dolor sit ametw wadawd dawdawd awdawd.</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </li>
+                        <li className="chatList__list__item">
+                            <div className="col-12 itemBlock p-0">
+                                <div className="row">
+                                    <div className="col-8">
+                                        <span className="chatId">#123123123</span>
+                                    </div>
+                                    <div className="col-4 chatTime text-right">10:20 pm</div>
+                                </div>
+                                <div className="row">
+                                    <div className="col-12 userName">Bogdan Aksenov</div>
+                                </div>
+                                <div className="row">
+                                    <div className="col-12">
+                                        <p className="userMessage">Lorem ipsum dolor sit ametw wadawd dawdawd awdawd.</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </li>
+                        <li className="chatList__list__item">
+                            <div className="col-12 itemBlock p-0">
+                                <div className="row">
+                                    <div className="col-8">
+                                        <span className="chatId">#123123123</span>
+                                    </div>
+                                    <div className="col-4 chatTime text-right">10:20 pm</div>
+                                </div>
+                                <div className="row">
+                                    <div className="col-12 userName">Bogdan Aksenov</div>
+                                </div>
+                                <div className="row">
+                                    <div className="col-12">
+                                        <p className="userMessage">Lorem ipsum dolor sit ametw wadawd dawdawd awdawd.</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </li>
+                        <li className="chatList__list__item">
+                            <div className="col-12 itemBlock p-0">
+                                <div className="row">
+                                    <div className="col-8">
+                                        <span className="chatId">#123123123</span>
+                                    </div>
+                                    <div className="col-4 chatTime text-right">10:20 pm</div>
+                                </div>
+                                <div className="row">
+                                    <div className="col-12 userName">Bogdan Aksenov</div>
+                                </div>
+                                <div className="row">
+                                    <div className="col-12">
+                                        <p className="userMessage">Lorem ipsum dolor sit ametw wadawd dawdawd awdawd.</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </li>
+                        <li className="chatList__list__item">
+                            <div className="col-12 itemBlock p-0">
+                                <div className="row">
+                                    <div className="col-8">
+                                        <span className="chatId">#123123123</span>
+                                    </div>
+                                    <div className="col-4 chatTime text-right">10:20 pm</div>
+                                </div>
+                                <div className="row">
+                                    <div className="col-12 userName">Bogdan Aksenov</div>
+                                </div>
+                                <div className="row">
+                                    <div className="col-12">
+                                        <p className="userMessage">Lorem ipsum dolor sit ametw wadawd dawdawd awdawd.</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </li>
+                        <li className="chatList__list__item">
+                            <div className="col-12 itemBlock p-0">
+                                <div className="row">
+                                    <div className="col-8">
+                                        <span className="chatId">#123123123</span>
+                                    </div>
+                                    <div className="col-4 chatTime text-right">10:20 pm</div>
+                                </div>
+                                <div className="row">
+                                    <div className="col-12 userName">Bogdan Aksenov</div>
+                                </div>
+                                <div className="row">
+                                    <div className="col-12">
+                                        <p className="userMessage">Lorem ipsum dolor sit ametw wadawd dawdawd awdawd.</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
             </div>
         </div>
     )
