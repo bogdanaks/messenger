1d5a336eaa20ce9ecb76ddd5c8016bfab85af2d9
bogdanaks <bogdanaks@bk.ru>
2020-08-05 19:54:37 +0300

    Create new component - ChatMessages

---
+++ client/src/components/ChatMessages/ChatMessages.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './styles.scss'
+
+const ChatMessages = () => {
+    return (
+        <div className="wrapperChatMessages">
+            <h1>Chat messages</h1>
+        </div>
+    )
+}
+
+export default ChatMessages
