b32111149b0ba749655229da09b14726a6478ddd
bogdanaks <bogdanaks@bk.ru>
2020-08-05 23:36:06 +0300

    Create new component - ChatInput

---
+++ client/src/components/ChatInput/ChatInput.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './styles.scss'
+
+const ChatInput = () => {
+    return (
+        <div>
+            <h1>Input comp.</h1>
+        </div>
+    )
+}
+
+export default ChatInput
