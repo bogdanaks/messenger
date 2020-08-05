e63e5c9c3ca8cf0848b4a13abdc1259ed77a589c
bogdanaks <bogdanaks@bk.ru>
2020-08-05 19:54:13 +0300

    Import page Chats

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react'
-import Main from './pages/Main/Main'
+// import Main from './pages/Main/Main'
+import Chats from './pages/Chats/Chats'
 
 function App() {
   return (
     <div className="App">
-      <Main />
+      {/* <Main /> */}
+      <Chats />
     </div>
   )
 }
