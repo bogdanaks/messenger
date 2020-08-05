87d235d5158ade348c68be547d5d91ed6705a82e
bogdanaks <bogdanaks@bk.ru>
2020-08-05 19:54:59 +0300

    Import components List and Messages

--- client/src/pages/Chats/Chats.jsx
+++ client/src/pages/Chats/Chats.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
 import './styles.scss'
 
+import ChatList from '../../components/ChatList/ChatList'
+import ChatMessages from '../../components/ChatMessages/ChatMessages'
+
 const Chats = () => {
     return (
         <div className="wrapperChats">
-            
+            <div className="container-fluid p-0">
+                <div className="row">
+                    <div className="col-3">
+                        <ChatList />
+                    </div>
+                    <div className="col-9">
+                        <ChatMessages />
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
