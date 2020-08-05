d93ada3a2f84330b266dd3d880b0d802f97cda5f
bogdanaks <bogdanaks@bk.ru>
2020-08-05 19:39:47 +0300

    Base style page

--- client/src/pages/Main/Main.jsx
+++ client/src/pages/Main/Main.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
-
+import "./styles.scss"
 const Main = () => {
     return (
-        <div>
-            <h1>Main page</h1>
+        <div className="container d-flex flex-column">
+            <div className="row no-gutters text-center align-items-center justify-content-center min-vh-100">
+                <div className="col-12 col-md-6 col-lg-5 col-xl-4">
+                    <h1>Create new chat</h1>
+                    <form class="mb-3">
+                        <div class="form-group">
+                            <label for="name" class="sr-only">Name</label>
+                            <input type="text" class="form-control form-control-md" id="name" placeholder="Enter your name"/>
+                        </div>
+                        <button class="btn btn-primary btn-lg btn-block text-uppercase font-weight-semibold" type="submit">CREATE</button>
+                    </form>
+                </div>
+            </div>
         </div>
     )
 }
