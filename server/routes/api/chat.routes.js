1e8ce35aa462ac82c9c7c0b39c5c4d72f0febffc
bogdanaks <bogdanaks@bk.ru>
2020-08-06 23:23:57 +0300

    Add route chat/:id and /create

--- server/routes/api/chat.routes.js
+++ server/routes/api/chat.routes.js
@@ -3,19 +3,34 @@ const router = Router()
 
 // Import Chat Model
 const Chats = require('../../models/Chats')
+// Import User Model
+const Users = require('../../models/Users')
 
 router.get('/', async (req, res) => {
     // console.log('Test')
 })
-router.get('/chats/:chatId', async (req, res) => {
+router.get('/:chatId', async (req, res) => {
     console.log(req.params.chatId)
+    try {
+        await Chats.find({chatId: req.params.chatId}, (err, items) => {
+            if(err) return res.status(404).send(err)
+            items.push()
+            return res.send(items)
+        })
+    } catch(err) {
+        res.status(400).send(err)
+    }
 })
 
 router.post('/create', async (req, res) => {
-    const newChat = new Chats({
-        chatId: req.body.id
-    })
     try {
+        const user = await Users.findOne({userId: req.body.userId})
+
+        const newChat = new Chats({
+            chatId: req.body.chatId,
+            users: user
+        })
+
         await newChat.save().then(chat => res.json(chat))
     } catch(err) {
         res.status(400).send(err)
