ef510af8663068d904e61920e856b3b5c01a2ae3
bogdanaks <bogdanaks@bk.ru>
2020-08-06 22:29:05 +0300

    Add route /login and /register

--- server/routes/api/user.routes.js
+++ server/routes/api/user.routes.js
@@ -1,19 +1,43 @@
 const { Router } = require('express')
 const router = Router()
 
-// Import Chat Model
+// Import Users Model
 const Users = require('../../models/Users')
 
-router.post('/create', async (req, res) => {
-    const newUser = new Users({
-        userId: req.body.id,
-        name: req.body.name,
-        password: req.body.password,
-    })
+router.post('/login', async (req, res) => {
     try {
-        await newUser.save().then(chat => res.json(chat))
+        // Checking if login exists
+        const user = await Users.findOne({name: req.body.name})
+        if(!user) return res.status(400).send('Login is not found')
+
+        // Check password
+        let validPassword
+        if(req.body.password === user.password) validPassword = true
+        if(!validPassword) return res.status(400).send('Invalid password')
+
+        res.send({userId: user.userId})
     } catch(err) {
         res.status(400).send(err)
+    }
+})
+
+
+router.post('/register', async (req, res) => {
+    // Checking if user is already in the db
+    const loginExist = await Users.findOne({name: req.body.name})
+    if(loginExist) return res.status(400).send('Login already exists')
+
+    // Add new user in database
+    const newUser = new Users({
+        userId: req.body.userId,
+        name: req.body.name,
+        password: req.body.password
+    })
+    try {
+        await newUser.save()
+        res.send({userId: newUser.userId})
+    } catch(err) {
+        res.status(400).send("Final error: "+ err)
     }
 })
 
