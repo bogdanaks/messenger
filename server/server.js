8232bbf1d513a080a78b5402f2c56ece7ceb2d65
bogdanaks <bogdanaks@bk.ru>
2020-08-06 20:14:19 +0300

    Create server and include routes

---
+++ server/server.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const cors = require('cors')
+const config = require('config')
+const http = require('http')
+// const socketio = require('socket.io')
+
+const app = express()
+const server = http.createServer(app)
+// const io = socketio(server)
+const PORT = process.env.PORT || config.get('port')
+
+app.use(cors())
+app.use(express.json())
+
+//routes
+app.use('/api/chat', require('./routes/api/chat.routes'))
+
+//start server
+async function start() {
+    try {
+        await mongoose.connect(config.get('mongoUri'), {useNewUrlParser: true, useUnifiedTopology: true})
+        await server.listen(PORT, () => console.log(`Server is running on port: ${PORT}`))
+    } catch (error) {
+        console.log(`Server error: ${error}`)
+        process.exit(1)
+    }
+}
+
+start()
