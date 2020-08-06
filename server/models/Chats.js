484b90db58a62eb8ef66cb6de575a3e2e3f179c0
bogdanaks <bogdanaks@bk.ru>
2020-08-06 20:43:13 +0300

    rename file

--- server/models/Chats.js
+++ server/models/Chats.js
@@ -1,10 +1,11 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
-const ChatSchema = new Schema({
+const ChatsSchema = new Schema({
     chatId: {
         type: String,
         required: true
     },
 }, { timestamps: true })
-module.exports = Room = mongoose.model('chat', ChatSchema)
+
+module.exports = Chat = mongoose.model('chats', ChatsSchema)
--- server/routes/api/chat.routes.js
+++ server/routes/api/chat.routes.js
@@ -2,13 +2,24 @@ const { Router } = require('express')
 const router = Router()
 
 // Import Chat Model
-const Chat = require('../../models/Chat')
+const Chats = require('../../models/Chats')
 
 router.get('/', async (req, res) => {
-    console.log('Test')
+    // console.log('Test')
 })
 router.get('/chats/:chatId', async (req, res) => {
     console.log(req.params.chatId)
+})
+
+router.post('/create', async (req, res) => {
+    const newChat = new Chats({
+        chatId: req.body.id
+    })
+    try {
+        await newChat.save().then(chat => res.json(chat))
+    } catch(err) {
+        res.status(400).send(err)
+    }
 })
 
 module.exports = router
