a6b57d6dcec0bcf42fc1bf9693cba07b4c88821a
bogdanaks <bogdanaks@bk.ru>
2020-08-06 18:17:32 +0300

    Bug fix

--- client/src/pages/Main/Main.jsx
+++ client/src/pages/Main/Main.jsx
@@ -6,12 +6,12 @@ const Main = () => {
             <div className="row no-gutters text-center align-items-center justify-content-center min-vh-100">
                 <div className="col-12 col-md-6 col-lg-5 col-xl-4">
                     <h1>Create new chat</h1>
-                    <form class="mb-3">
-                        <div class="form-group">
-                            <label for="name" class="sr-only">Name</label>
-                            <input type="text" class="form-control form-control-md" id="name" placeholder="Enter your name"/>
+                    <form className="mb-3">
+                        <div className="form-group">
+                            <label htmlFor="name" className="sr-only">Name</label>
+                            <input type="text" className="form-control form-control-md" id="name" placeholder="Enter your name"/>
                         </div>
-                        <button class="btn btn-primary btn-lg btn-block text-uppercase font-weight-semibold" type="submit">CREATE</button>
+                        <button className="btn btn-primary btn-lg btn-block text-uppercase font-weight-semibold" type="submit">CREATE</button>
                     </form>
                 </div>
             </div>
