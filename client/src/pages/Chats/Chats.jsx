bcd5d74691a90c2c313a16f910edd6ab503db326
bogdanaks <bogdanaks@bk.ru>
2020-08-06 18:09:38 +0300

    Check if the chat is selected

--- client/src/pages/Chats/Chats.jsx
+++ client/src/pages/Chats/Chats.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { useParams } from 'react-router-dom'
 import './styles.scss'
 
 import ChatHeader from '../../components/ChatHeader/ChatHeader'
@@ -7,20 +8,28 @@ import ChatMessages from '../../components/ChatMessages/ChatMessages'
 import ChatInput from '../../components/ChatInput/ChatInput'
 
 const Chats = () => {
+    const { id } = useParams()
     return (
         <div className="wrapperChats">
             <div className="container-fluid p-0">
-                <ChatHeader />
+                <ChatHeader id={id} />
                 <div className="row no-gutters">
                     <div className="col-3">
-                        <ChatList />
+                        <ChatList id={id} />
                     </div>
                     <div className="col-9">
                         <div className="container-fluid">
-                            <div className="row"><ChatMessages /></div>
-                            <div className="row"><ChatInput /></div>
+                            {id ? 
+                                <>
+                                <div className="row"><ChatMessages id={id} /></div>
+                                <div className="row"><ChatInput id={id} /></div>
+                                </>
+                                :
+                                <div className="chatNot">
+                                    <h4>Chat not selected</h4>
+                                </div>
+                            }
                         </div>
-
                     </div>
                 </div>
             </div>
