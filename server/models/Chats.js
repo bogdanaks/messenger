f746ac85b7959e569445a467e62497e16486af34
bogdanaks <bogdanaks@bk.ru>
2020-08-06 23:24:25 +0300

    Add fields users and messages

--- server/models/Chats.js
+++ server/models/Chats.js
@@ -6,6 +6,14 @@ const ChatsSchema = new Schema({
         type: String,
         required: true
     },
+    users: {
+        type: Array
+    },
+    messages: [{
+        senderId: String,
+        senderName: String,
+        text: String
+    }, { timestamps: true }],
 }, { timestamps: true })
 
 module.exports = Chat = mongoose.model('chats', ChatsSchema)
