73707243263956210b24e691fbac9ef4f6e8be40
bogdanaks <bogdanaks@bk.ru>
2020-08-06 20:42:52 +0300

    Create page Auth

---
+++ client/src/pages/Auth/Auth.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import './styles.scss'
+
+const Auth = () => {
+    return (
+        <div className="authWrapper">
+            
+        </div>
+    )
+}
+
+export default Auth
