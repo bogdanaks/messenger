4ab9c841f195c9949377adc7a32d02a4e2bd5449
bogdanaks <bogdanaks@bk.ru>
2020-08-07 14:17:49 +0300

    Change import route

--- client/src/pages/Chats/Chats.jsx
+++ client/src/pages/Chats/Chats.jsx
@@ -1,8 +1,8 @@
 import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
-import socket from '../../socket'
+import socket from '../../utils/socket'
 
-import api from '../../axios'
+import api from '../../utils/axios'
 
 import './styles.scss'
 
