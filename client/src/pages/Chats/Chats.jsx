4e07e30e581a38f36b5d615d82c62e457bbf4043
bogdanaks <bogdanaks@bk.ru>
2020-08-07 17:16:37 +0300

    Clear spaces

--- client/src/pages/Chats/Chats.jsx
+++ client/src/pages/Chats/Chats.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
-import socket from '../../utils/socket'
 
+import socket from '../../utils/socket'
 import api from '../../utils/axios'
 
 import './styles.scss'
