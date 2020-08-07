ed264f06fafe78d50d317232ad95f076d78c5103
bogdanaks <bogdanaks@bk.ru>
2020-08-07 17:17:06 +0300

    Decompose logic to redux action

--- client/src/components/ChatHeader/ChatHeader.jsx
+++ client/src/components/ChatHeader/ChatHeader.jsx
@@ -1,5 +1,8 @@
 import React from 'react'
+import { useDispatch } from 'react-redux'
 import { useHistory } from 'react-router-dom'
+
+import { createChat } from '../../redux/actions'
 import './styles.scss'
 
 import usersImg from '../../assets/users.png'
@@ -7,17 +10,24 @@ import userAddImg from '../../assets/userAdd.png'
 import exitImg from '../../assets/exit.png'
 
 const ChatHeader = ({ id }) => {
+    const dispatch = useDispatch()
     const history = useHistory()
     const handleExitClick = () => {
         localStorage.clear('auth')
         history.push('/login')
+    }
+    const handleCreateChat = (e) => {
+        e.preventDefault()
+        const chatId = Date.now()
+        dispatch(createChat(chatId))
+        history.push('/chats/'+chatId)
     }
     return (
         <div className="row chatList__header no-gutters">
             <div className="col-3 chatTitle">
                 <h4>Chats</h4>
                 <div className="chatTitle__createChat">
-                    <span>+</span>
+                    <span onClick={handleCreateChat}>+</span>
                 </div>
                 <div className="exitImg">
                     <img src={exitImg} alt="Exit" onClick={handleExitClick}/>
