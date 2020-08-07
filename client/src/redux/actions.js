17d7dfb890b2ea6d70d30e68e2eb9b01a4d7a2c3
bogdanaks <bogdanaks@bk.ru>
2020-08-07 17:16:11 +0300

    Add actions

---
+++ client/src/redux/actions.js
@@ -0,0 +1,89 @@
+import { GET_CHATS, GET_LAST_MSG, GET_LAST_MSG_ONE } from "./actionTypes"
+import socket from '../utils/socket'
+import api from '../utils/axios'
+
+
+// Chats actions
+export function getChats() {
+    return async dispatch => {
+        try {
+            await api.get(`/api/user/getChats/${JSON.parse(localStorage.getItem('auth')).userId}`)
+                .then( res => (
+                    dispatch({ type: GET_CHATS, payload: res.data.inChats })
+                ))
+                .catch(err => alert(err))
+        } catch(err) {
+            alert(err)
+        }
+    }
+}
+
+export function getLastMsgs() {
+    return async dispatch => {
+        try {
+            getChatsList().then( async (data) => {
+                await api.get(`/api/message/getLastMsg/${data}`)
+                .then(res => {
+                    dispatch({ type: GET_LAST_MSG, payload: res.data })
+                    res.data.forEach((el) => {
+                        socket.joinChat(el.chatId)
+                    })
+                })
+                .catch(err => alert(err))
+            })
+
+        } catch (err) {
+            alert(err)
+        }
+    }
+}
+
+export function getMsgByChatId(chatId) {
+    return async dispatch => {
+        try {
+            await api.get(`/api/message/getMsgById/${chatId}`)
+            .then(res => {
+                dispatch({ type: GET_LAST_MSG_ONE, payload: res.data })
+                socket.joinChat(res.data[0].chatId)
+            })
+            .catch(err => alert(err))
+        } catch (err) {
+            alert(err)
+        }
+    }
+}
+
+export function createChat(chatId) {
+    return async dispatch => {
+        try {
+            const chatObj = {
+                chatId,
+                userId: JSON.parse(localStorage.getItem('auth')).userId
+            }
+            await api.post('/api/chat/create', chatObj)
+                .then(res => {
+                    dispatch(getMsgByChatId(res.data.chatId))
+                })
+                .catch(err => alert(err) )
+        } catch (error) {
+            alert(error)
+        }
+    }
+}
+
+
+
+
+
+
+
+
+const getChatsList = async () => {
+    try {
+        return await api.get(`/api/user/getChats/${JSON.parse(localStorage.getItem('auth')).userId}`)
+            .then( res => res.data.inChats)
+            .catch(err => alert(err))
+    } catch(err) {
+        alert(err)
+    }
+}
