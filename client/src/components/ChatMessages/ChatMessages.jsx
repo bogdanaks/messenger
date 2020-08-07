af57ee2711f9a990062f968606112476763e31e2
bogdanaks <bogdanaks@bk.ru>
2020-08-07 11:29:17 +0300

    Set data in jsx

--- client/src/components/ChatMessages/ChatMessages.jsx
+++ client/src/components/ChatMessages/ChatMessages.jsx
@@ -20,7 +20,7 @@ const ChatMessages = ({ id }) => {
                         <li key={item._id}>
                             <div className="row messagesBlock__message messageSelf">
                                 <div className="row">
-                                    <div className="col-8"><div className="col-12 messagesBlock__message__user">{item.userId}</div></div>
+                                    <div className="col-8"><div className="col-12 messagesBlock__message__user">{item.userName}</div></div>
                                     <div className="col-4"><div className="messageTime">10:20 pm</div></div>
                                 </div>
                                 <div className="col-12 messagesBlock__message__text">{item.text}</div>
