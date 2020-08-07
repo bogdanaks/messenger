2297b4fa3f498bf97510aec8fdd57913e9d518c9
bogdanaks <bogdanaks@bk.ru>
2020-08-07 10:59:26 +0300

    Decompose logic from Chat

--- client/src/components/ChatMessages/ChatMessages.jsx
+++ client/src/components/ChatMessages/ChatMessages.jsx
@@ -1,101 +1,33 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import api from '../../axios'
 import './styles.scss'
 
 const ChatMessages = ({ id }) => {
+    const [messages, setMessages] = useState([])
+    useEffect(() => {
+        api.get(`/api/message/getMsgById/${id}`)
+        .then(res => {
+            setMessages(res.data)
+        })
+        .catch(err => alert(err))
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [])
     return (
         <div className="wrapperChatMessages">
             <div className="container-fluid messagesBlock">
                 <ul>
-                    <li>
-                        <div className="row messagesBlock__message">
-                            <div className="row">
-                                <div className="col-8"><div className="col-12 messagesBlock__message__user">Bogdan Aksenov</div></div>
-                                <div className="col-4"><div className="messageTime">10:20 pm</div></div>
+                    {messages.map((item, indx) => (
+                        <li key={item._id}>
+                            <div className="row messagesBlock__message messageSelf">
+                                <div className="row">
+                                    <div className="col-8"><div className="col-12 messagesBlock__message__user">{item.userId}</div></div>
+                                    <div className="col-4"><div className="messageTime">10:20 pm</div></div>
+                                </div>
+                                <div className="col-12 messagesBlock__message__text">{item.text}</div>
                             </div>
-                            <div className="col-12 messagesBlock__message__text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto reprehenderit non ut, animi ipsam at?</div>
-                        </div>
-                    </li>
-                    <li>
-                        <div className="row messagesBlock__message messageSelf">
-                            <div className="row">
-                                <div className="col-8"><div className="col-12 messagesBlock__message__user">Bogdan Aksenov</div></div>
-                                <div className="col-4"><div className="messageTime">10:20 pm</div></div>
-                            </div>
-                            <div className="col-12 messagesBlock__message__text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto reprehenderit non ut, animi ipsam at?</div>
-                        </div>
-                    </li>
-                    <li>
-                        <div className="row messagesBlock__message">
-                            <div className="row">
-                                <div className="col-8"><div className="col-12 messagesBlock__message__user">Bogdan Aksenov</div></div>
-                                <div className="col-4"><div className="messageTime">10:20 pm</div></div>
-                            </div>
-                            <div className="col-12 messagesBlock__message__text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto reprehenderit non ut, animi ipsam at?</div>
-                        </div>
-                    </li>
-                    <li>
-                        <div className="row messagesBlock__message">
-                            <div className="row">
-                                <div className="col-8"><div className="col-12 messagesBlock__message__user">Bogdan Aksenov</div></div>
-                                <div className="col-4"><div className="messageTime">10:20 pm</div></div>
-                            </div>
-                            <div className="col-12 messagesBlock__message__text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto reprehenderit non ut, animi ipsam at?</div>
-                        </div>
-                    </li>
-                    <li>
-                        <div className="row messagesBlock__message">
-                            <div className="row">
-                                <div className="col-8"><div className="col-12 messagesBlock__message__user">Bogdan Aksenov</div></div>
-                                <div className="col-4"><div className="messageTime">10:20 pm</div></div>
-                            </div>
-                            <div className="col-12 messagesBlock__message__text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto reprehenderit non ut, animi ipsam at?</div>
-                        </div>
-                    </li>
-                    <li>
-                        <div className="row messagesBlock__message">
-                            <div className="row">
-                                <div className="col-8"><div className="col-12 messagesBlock__message__user">Bogdan Aksenov</div></div>
-                                <div className="col-4"><div className="messageTime">10:20 pm</div></div>
-                            </div>
-                            <div className="col-12 messagesBlock__message__text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto reprehenderit non ut, animi ipsam at?</div>
-                        </div>
-                    </li>
-                    <li>
-                        <div className="row messagesBlock__message">
-                            <div className="row">
-                                <div className="col-8"><div className="col-12 messagesBlock__message__user">Bogdan Aksenov</div></div>
-                                <div className="col-4"><div className="messageTime">10:20 pm</div></div>
-                            </div>
-                            <div className="col-12 messagesBlock__message__text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto reprehenderit non ut, animi ipsam at?</div>
-                        </div>
-                    </li>
-                    <li>
-                        <div className="row messagesBlock__message messageSelf">
-                            <div className="row">
-                                <div className="col-8"><div className="col-12 messagesBlock__message__user">Bogdan Aksenov</div></div>
-                                <div className="col-4"><div className="messageTime">10:20 pm</div></div>
-                            </div>
-                            <div className="col-12 messagesBlock__message__text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto reprehenderit non ut, animi ipsam at?</div>
-                        </div>
-                    </li>
-                    <li>
-                        <div className="row messagesBlock__message messageSelf">
-                            <div className="row">
-                                <div className="col-8"><div className="col-12 messagesBlock__message__user">Bogdan Aksenov</div></div>
-                                <div className="col-4"><div className="messageTime">10:20 pm</div></div>
-                            </div>
-                            <div className="col-12 messagesBlock__message__text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto reprehenderit non ut, animi ipsam at?</div>
-                        </div>
-                    </li>
-                    <li>
-                        <div className="row messagesBlock__message">
-                            <div className="row">
-                                <div className="col-8"><div className="col-12 messagesBlock__message__user">Bogdan Aksenov</div></div>
-                                <div className="col-4"><div className="messageTime">10:20 pm</div></div>
-                            </div>
-                            <div className="col-12 messagesBlock__message__text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto reprehenderit non ut, animi ipsam at?</div>
-                        </div>
-                    </li>
+                        </li>
+                    ))}
+
                 </ul>
             </div>
         </div>
