b8c07fdbfb9f0a6911b7b973e178e93982d889d6
bogdanaks <bogdanaks@bk.ru>
2020-08-07 17:16:53 +0300

    Decompose logic to redux action

--- client/src/components/ChatList/ChatList.jsx
+++ client/src/components/ChatList/ChatList.jsx
@@ -1,26 +1,17 @@
-import React, { useState, useEffect } from 'react'
+import React, { useEffect } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
 import { useHistory } from 'react-router-dom'
 
-import socket from '../../socket'
-import api from '../../axios'
+import { getLastMsgs } from '../../redux/actions'
 import './styles.scss'
 
 const ChatList = ({ id }) => {
-    const [chatsList, setChatsList] = useState([])
+    const dispatch = useDispatch()
+    const lastMsgList = useSelector(state => state.chats.lastMsgList)
     const history = useHistory()
+
     useEffect(() => {
-        api.get(`/api/user/getChats/${JSON.parse(localStorage.getItem('auth')).userId}`)
-            .then( async (res) => {
-                await api.get(`/api/message/getLastMsg/${res.data.inChats}`)
-                    .then(res => {
-                        setChatsList(res.data)
-                        res.data.forEach((el) => {
-                            socket.joinChat(el.chatId)
-                        })
-                    })
-                    .catch(err => alert(err))
-            })
-            .catch(err => alert(err))
+        dispatch(getLastMsgs())
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [])
     const handleChatClick = (chatId) => {
@@ -31,8 +22,8 @@ const ChatList = ({ id }) => {
             <div className="container-fluid p-0">
                 <div className="row chatList__list no-gutters mh-70">
                     <ul className="chatList__content">
-                        {chatsList.map((item , index) => (
-                        <li 
+                        {lastMsgList.map((item , index) => (
+                        <li
                             className={item.chatId === id ? "chatList__list__item active" : "chatList__list__item"} 
                             key={item.chatId} 
                             onClick={() => handleChatClick(item.chatId)}
