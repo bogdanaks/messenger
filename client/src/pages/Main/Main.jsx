63587c66d59487a774911881aab9c6bc8f6a5501
bogdanaks <bogdanaks@bk.ru>
2020-08-07 14:17:38 +0300

    Change import route

--- client/src/pages/Main/Main.jsx
+++ client/src/pages/Main/Main.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react'
 import { useHistory } from 'react-router-dom'
-import api from '../../axios'
+import api from '../../utils/axios'
 
 import "./styles.scss"
 
