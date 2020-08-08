6f93b4296a0f853a108243bc62d5946884414997
bogdanaks <bogdanaks@bk.ru>
2020-08-08 19:25:33 +0300

    Add type INIT_MESSAGE

--- client/src/redux/chatsReducer.js
+++ client/src/redux/chatsReducer.js
@@ -1,4 +1,4 @@
-import { GET_CHATS, GET_LAST_MSG, GET_LAST_MSG_ONE, SET_MESSAGE } from './actionTypes'
+import { GET_CHATS, GET_LAST_MSG, GET_LAST_MSG_ONE, SET_MESSAGE, INIT_MESSAGE } from './actionTypes'
 
 const initialState = {
     chatList: [],
@@ -14,6 +14,8 @@ export const chatsReducer = (state = initialState, action) => {
             return { ...state, lastMsgList: state.lastMsgList.concat(action.payload) }
         case GET_LAST_MSG_ONE:
             return { ...state, lastMsgList: state.lastMsgList.concat(action.payload) }
+        case INIT_MESSAGE:
+            return { ...state, messages: [...action.payload] }
         case SET_MESSAGE:
             return { ...state, messages: state.messages.concat(action.payload) }
         default: return state
