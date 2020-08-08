dd87ec336e3f353b1d6366178d01ce12bfb60e17
bogdanaks <bogdanaks@bk.ru>
2020-08-08 19:26:57 +0300

    Dispatch action setMessage

--- client/src/components/ChatInput/ChatInput.jsx
+++ client/src/components/ChatInput/ChatInput.jsx
@@ -1,7 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useDispatch } from 'react-redux'
+
+import { sendMessage } from '../../redux/actions'
 import './styles.scss'
 
-const ChatInput = () => {
+const ChatInput = ({ id }) => {
+    const [message, setMessage] = useState('')
+    const dispatch = useDispatch()
+    const handleTextareaChange = (e) => {
+        setMessage(e.target.value)
+    }
+    const handleSendClick = () => {
+        dispatch(sendMessage(message, id))
+        setMessage('')
+    }
     return (
         <div className="container-fluid">
            <div className="row">
@@ -9,12 +21,12 @@ const ChatInput = () => {
                    <div className="row">
                        <div className="col">
                             <div className="inputText">
-                                <textarea className="form-control transparent-bg border-0" placeholder="Write your message..."></textarea>
+                                <textarea className="form-control transparent-bg border-0" value={message} placeholder="Write your message..." onChange={handleTextareaChange}></textarea>
                             </div>
                        </div>
                        <div className="col-auto">
                             <div className="inputSend">
-                                <button>SEND</button>
+                                <button onClick={handleSendClick}>SEND</button>
                             </div>
                        </div>
                    </div>
