f802f6e5917ac9acf64a9b00acf110dc623810e6
bogdanaks <bogdanaks@bk.ru>
2020-08-08 18:56:27 +0300

    Add type SET_MESSAGE

--- client/src/redux/actionTypes.js
+++ client/src/redux/actionTypes.js
@@ -1,3 +1,5 @@
-export const GET_CHATS = 'CHAT/GET_CHATS'
-export const GET_LAST_MSG = 'CHAT/GET_LAST_MSG'
-export const GET_LAST_MSG_ONE = 'CHAT/GET_LAST_MSG_ONE'
+export const GET_CHATS          = 'CHAT/GET_CHATS'
+export const GET_LAST_MSG       = 'CHAT/GET_LAST_MSG'
+export const GET_LAST_MSG_ONE   = 'CHAT/GET_LAST_MSG_ONE'
+
+export const SET_MESSAGE        = 'MESSAGE/SET_MESSAGE'
