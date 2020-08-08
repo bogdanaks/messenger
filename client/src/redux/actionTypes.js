66d14f024d6500ef13bbabd60a610fe257f0c26e
bogdanaks <bogdanaks@bk.ru>
2020-08-08 20:34:11 +0300

    Rename

--- client/src/redux/actionTypes.js
+++ client/src/redux/actionTypes.js
@@ -1,6 +1,6 @@
 export const GET_CHATS          = 'CHAT/GET_CHATS'
 export const GET_LAST_MSG       = 'CHAT/GET_LAST_MSG'
-export const GET_LAST_MSG_ONE   = 'CHAT/GET_LAST_MSG_ONE'
+export const SET_LAST_MSG       = 'CHAT/SET_LAST_MSG'
 
 export const SET_MESSAGE        = 'MESSAGE/SET_MESSAGE'
 export const INIT_MESSAGE       = 'MESSAGE/INIT_MESSAGE'
