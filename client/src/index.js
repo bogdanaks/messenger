e7ac72373fa324a7abecd347bb6a5e1bee36d6a5
bogdanaks <bogdanaks@bk.ru>
2020-08-08 20:33:26 +0300

    Import redux devtoolz

--- client/src/index.js
+++ client/src/index.js
@@ -8,7 +8,8 @@ import { Provider } from 'react-redux'
 import thunk from 'redux-thunk'
 
 const store = createStore(rootReducer, compose(
-  applyMiddleware(thunk)
+  applyMiddleware(thunk),
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 ))
 
 const app = (
