f5e1da7f683ead36e5e92cd09970f06388cb9d2a
bogdanaks <bogdanaks@bk.ru>
2020-08-08 22:10:35 +0300

    Add new type

--- client/src/redux/actionTypes.js
+++ client/src/redux/actionTypes.js
@@ -3,4 +3,5 @@ export const GET_LAST_MSG       = 'CHAT/GET_LAST_MSG'
 export const SET_LAST_MSG       = 'CHAT/SET_LAST_MSG'
 
 export const SET_MESSAGE        = 'MESSAGE/SET_MESSAGE'
+export const SET_MESSAGE_STORE  = 'MESSAGE/SET_MESSAGE_STORE'
 export const INIT_MESSAGE       = 'MESSAGE/INIT_MESSAGE'
