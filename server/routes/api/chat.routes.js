3b892b8968f2553be08efa7156099a93a492a303
bogdanaks <bogdanaks@bk.ru>
2020-08-08 23:42:11 +0300

    Clear

--- server/routes/api/chat.routes.js
+++ server/routes/api/chat.routes.js
@@ -8,9 +8,6 @@ const Users = require('../../models/Users')
 // Import Message Model
 const Messages = require('../../models/Messages')
 
-router.get('/', async (req, res) => {
-    // console.log('Test')
-})
 router.get('/getUserNamesIn/:chatId', async (req, res) => {
     try {
         await Chats.find({chatId: req.params.chatId}, (err, items) => {
@@ -48,6 +45,20 @@ router.post('/create', async (req, res) => {
         user.save()
 
         await newChat.save().then(chat => res.send(chat))
+    } catch(err) {
+        res.status(400).send(err)
+    }
+})
+
+router.post('/enterchat', async (req, res) => {
+    try {
+        const user = await Users.findOne({userId: Number(req.body.userId)})
+        user.inChats.push(req.body.chatId)
+        await user.save()
+
+        const chat = await Chats.findOne({chatId: req.body.chatId})
+        chat.users.push(user)
+        await chat.save().then(chat => res.send(chat))
     } catch(err) {
         res.status(400).send(err)
     }
