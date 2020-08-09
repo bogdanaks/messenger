7c9025de77e5825579828cd8feaec552a38f2429
bogdanaks <bogdanaks@bk.ru>
2020-08-09 16:21:29 +0300

    Create page 404

---
+++ client/src/pages/Page404/Page404.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Page404 = () => {
+    return (
+        <div className="container d-flex flex-column">
+            <div className="row no-gutters text-center align-items-center justify-content-center min-vh-100">
+                <div className="col-12 col-md-6 col-lg-5 col-xl-4">
+                    <h1>404 Page not Found</h1>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Page404
