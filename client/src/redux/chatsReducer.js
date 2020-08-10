904637eb9cd4c34fe00321387c0fe0bbc8489780
bogdanaks <bogdanaks@bk.ru>
2020-08-11 00:58:59 +0300

    Removing unnecessary

--- client/src/redux/chatsReducer.js
+++ client/src/redux/chatsReducer.js
@@ -1,44 +1,31 @@
-import { GET_USERS_IN_CHAT, INIT_USERS_ONLINE, SET_USERS_ONLINE, GET_LAST_MSG, SET_LAST_MSG, SET_MESSAGE, SET_MESSAGE_STORE, INIT_MESSAGE } from './actionTypes'
+import { GET_CHATS_USER,
+        SET_NEW_MESSAGE,
+        INIT_MESSAGE,
+        SET_USERS_ONLINE } from './actionTypes'
 
 const initialState = {
-    usersOnline: [],
-    usersInChat: [],
-    lastMsgList: [],
-    messages: []
+    chatsUser: [],
+    messages: []    
 }
 
 export const chatsReducer = (state = initialState, action) => {
     switch(action.type) {
-        case GET_USERS_IN_CHAT:
-            return { ...state, usersInChat: action.payload }
-        case INIT_USERS_ONLINE:
-            const initonlineArr = []
-            action.payload.forEach(element => {
-                initonlineArr.push(element.userId)
-            })
-            return { ...state,  usersOnline: initonlineArr}
+        case GET_CHATS_USER:
+            return { ...state,  chatsUser: action.payload}
         case SET_USERS_ONLINE:
-            const onlineArr = []
-            action.payload.forEach(element => {
-                onlineArr.push(element.userId)
-            })
-            return { ...state,  usersOnline: onlineArr}
-        case GET_LAST_MSG:
-            return { ...state, lastMsgList: state.lastMsgList.concat(action.payload) }
-        case SET_LAST_MSG:            
-            const newList = state.lastMsgList.map(obj => {
-                if (obj.chatId === action.payload.chatId) {
-                    return action.payload
-                }
-                return obj
-            })
-            return { ...state, lastMsgList: newList }
+            return { ...state,  usersOnline: action.payload}
+
         case INIT_MESSAGE:
             return { ...state, messages: [...action.payload] }
-        case SET_MESSAGE:
-            return { ...state, messages: state.messages.concat(action.payload) }
-        case SET_MESSAGE_STORE:
-            return { ...state, messages: state.messages.concat(action.payload) }
+        case SET_NEW_MESSAGE:
+            let newArr = state.chatsUser.map((chat) => {
+                if(chat.chatId === action.payload.chatId) {
+                    chat.lastMsg = action.payload
+                    return chat
+                }
+                return chat
+            })
+            return { ...state, chatsUser: newArr, messages: state.messages.concat(action.payload) }
         default: return state
     }
 }
